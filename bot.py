2cd98bd6caca51df5b031932f833b66165a17202
insane-man220 <76524189+insane-man220@users.noreply.github.com>
2021-04-09 22:18:08 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+
+API_ ID = 'YOUR API ID'
+API_HASH = 'YOUR API HASH'
+STRING_SESSION = 'USERBOT STING SESSION'
+CHANNEL = 'PRIVATE CHANNEL USERNAME OR ID'
+
+bot = Client(STRING_SESSION, API_ID, API_HASH)
+
+
+@bot.on_message(filters.incoming & filters.channel & ~filters.chat(CHANNEL))
+async def forward(bot, message):
+    await message.forward(CHANNEL)
+
+
+bot.run()
