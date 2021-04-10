f772821365ae4e1a25cb3de1579064c208fe52f7
insane-man220 <76524189+insane-man220@users.noreply.github.com>
2021-04-10 21:04:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
+from os import environ as env
 from pyrogram import Client, filters
 
-API_ID = 'YOUR API ID'
-API_HASH = 'YOUR API HASH'
-STRING_SESSION = 'USERBOT STING SESSION'
-CHANNEL = 'PRIVATE CHANNEL USERNAME OR ID'
+API_ID = env['API_ID']
+API_HASH = env['API_HASH']
+USERBOT_STRING_SESSION = env['USERBOT_STRING_SESSION']
+CHANNEL = int(env['CHANNEL_ID'])
 
-bot = Client(STRING_SESSION, API_ID, API_HASH)
+bot = Client(USERBOT_STRING_SESSION, API_ID, API_HASH)
 
 
 @bot.on_message(filters.incoming & filters.channel & ~filters.chat(CHANNEL))
